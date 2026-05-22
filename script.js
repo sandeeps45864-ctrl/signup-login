1eb4ccb9a105011a0f326ddfdb019ea9b840eeb2
Sandeep <sandeeps45864@gamil.com>
2026-05-22 16:17:59 +0530

    final duplicate fix

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
 console.log("JS Loaded");
 
-document.querySelector('#contactForm').addEventListener('submit', async (e) => {
-
-    e.preventDefault();
-
-    console.log("Button Clicked");
-
-    const name = document.querySelector('#fname').value;
-    const email = document.querySelector('#email').value;
-    const message = document.querySelector('#message').value;
-
-    try {
-
-        const response = await fetch("https://netlify-website-74pi.onrender.com/send", {
-
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json",
-            },
-
-            body: JSON.stringify({
-                name,
-                email,
-                message
-            })
-
-        });
-
-        const data = await response.text();
-
-        console.log(data);
-
-        alert(data);
-
-    } catch (error) {
-
-        console.log("FRONTEND ERROR:", error);
-
-        alert("Error sending message");
-
-    }
-
-});
-
-
-
-console.log("JS Loaded");
-
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", async function (e) {
@@ -56,8 +8,12 @@ form.addEventListener("submit", async function (e) {
 
     console.log("Form Submitted");
 
-    const name = document.getElementById("fname").value;
+    const name =
+        document.getElementById("fname").value + " " +
+        document.getElementById("lname").value;
+
     const email = document.getElementById("email").value;
+
     const message = document.getElementById("message").value;
 
     try {
@@ -86,7 +42,7 @@ form.addEventListener("submit", async function (e) {
 
     } catch (error) {
 
-        console.log("ERROR:", error);
+        console.log("FRONTEND ERROR:", error);
 
         alert("Message Failed");
 
