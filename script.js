c47fce9f17d2c413d6f883c9f40eabf5757a70b4
Sandeep <sandeeps45864@gamil.com>
2026-05-22 16:12:25 +0530

    saved js fix

--- script.js
+++ script.js
@@ -43,3 +43,53 @@ document.querySelector('#contactForm').addEventListener('submit', async (e) => {
     }
 
 });
+
+
+
+console.log("JS Loaded");
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    console.log("Form Submitted");
+
+    const name = document.getElementById("fname").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    try {
+
+        const response = await fetch("https://netlify-website-74pi.onrender.com/send", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({
+                name,
+                email,
+                message
+            })
+
+        });
+
+        const data = await response.text();
+
+        console.log("SERVER RESPONSE:", data);
+
+        alert(data);
+
+    } catch (error) {
+
+        console.log("ERROR:", error);
+
+        alert("Message Failed");
+
+    }
+
+});
